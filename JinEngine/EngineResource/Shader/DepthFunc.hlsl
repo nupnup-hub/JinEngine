161eecd6877984ae598a658bb15817b8561b0777
nupnup-hub <59456231+nupnup-hub@users.noreply.github.com>
2023-09-13 17:53:49 +0900

    Delete JinEngine/EngineResource directory

--- JinEngine/EngineResource/Shader/DepthFunc.hlsl
+++
@@ -1,9 +0,0 @@
-
-float ToLinearZValue(const float v, const float camNear, const float camFar)
-{
-	return (2.0f * camNear) / (camNear + camFar - v * (camFar - camNear));
-}
-float ToNoLinearZValue(const float v, const float camNear, const float camFar)
-{
-	return  -((camNear + camFar) * v - (2 * camNear)) / ((camNear - camFar) * v);
-}
